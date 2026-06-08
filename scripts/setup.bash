3142f132a48eaea208686974b40f542d5923e0d2
Pieter van der Zijden <pieter@proculair.com>
2026-06-08 19:07:09 +0200

    changed scripts to work with macOS

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -9,5 +9,5 @@ export ROS_MASTER_URI="http://localhost:11311"
 #    hostname -I | awk '{print $1}'
 # macOS:
 #    ipconfig getifaddr en1
-export COPPELIA_SIM_IP="192.168.0.241"
+export COPPELIA_SIM_IP="192.168.2.15"
  
